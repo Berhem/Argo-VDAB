327c605b5e170e107141b6226f336ba8f0efc7a3
Berhem <berhemisik@Berhems-MacBook-Pro.local>
2015-03-30 10:36:08 +0200

    Backup svn
    backUp van svn

---
+++ ArgoVDAB/www/js/app.js
@@ -0,0 +1,52 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+    .controller('MapController', function($scope, $ionicLoading) {
+
+        google.maps.event.addDomListener(window, 'load', function() {
+            var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+
+            var mapOptions = {
+                center: myLatlng,
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                disableDefaultUI: true
+            };
+
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                var myLocation = new google.maps.Marker({
+                    position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                    map: map,
+                    title: "My Location"
+                });
+            });
+
+            var trafficLayer = new google.maps.TrafficLayer();
+            trafficLayer.setMap(map);
+
+
+            $scope.map = map;
+        });
+
+
+    });
